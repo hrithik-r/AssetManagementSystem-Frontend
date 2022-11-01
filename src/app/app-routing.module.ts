3872b2a65ddeb4131cd807b884c622ea4bad0f85
Akshay <pawarar7799@gmail.com>
2022-11-01 23:20:01 +0530

    adding admin component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {VendorComponent} from '../../src/app/vendor/vendor/vendor.component'
 import {VendorlistComponent} from 'src/app/vendor/vendorlist/vendorlist.component'
-<<<<<<< HEAD
+
 import { VendorentryComponent } from './vendor/vendorentry/vendorentry.component';
 import { AdminComponent } from 'src/app/admin/admin.component'
-=======
+
 import {AssetCreationComponent} from  './asset/asset-creation/asset-creation.component'
->>>>>>> 0cc14b11b47a1d6dfa97bdf092afd849622f62cf
+
 
 
 const routes: Routes = [
   { path: 'vendor', component:  VendorComponent},
   { path: 'vendorlist', component:  VendorlistComponent},
-<<<<<<< HEAD
+
   { path: 'vendorentry', component:  VendorentryComponent},
   { path: 'admin', component:  AdminComponent},
-=======
+
   { path: 'assets',component: AssetCreationComponent}
->>>>>>> 0cc14b11b47a1d6dfa97bdf092afd849622f62cf
+
 ];
 
 @NgModule({
