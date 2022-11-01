47148c2013f6d6f23c778c2d99c54cc064bba590
hrithik <hrithikmike10@gmail.com>
2022-11-01 12:28:40 +0530

    made changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VendorComponent } from './vendor/vendor.component';
-import { AssetCreationComponent } from './asset-creation/asset-creation.component';
-import { AssetDefinitionComponent } from './asset-definition/asset-definition.component';
-import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    VendorComponent,
-    AssetCreationComponent,
-    AssetDefinitionComponent,
-    PurchaseOrderComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
